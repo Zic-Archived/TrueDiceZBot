71c5e3508fd583b340883902caa245ddda831842
zickieloox <zickieloox@gmail.com>
2018-06-20 23:32:19 +0700

    - Add strategy "Winswitch" - Add "nonstop loses limit" - Add statistics feature

--- ftp/truedice.js
+++ ftp/truedice.js
@@ -1,7 +1,8 @@
 "uses strict";
 
-var VERSION_NAME = 'V1.4';
-var VERSION_CODE = 14;
+var VERSION_NAME = 'V1.6';
+var VERSION_CODE = 16;
+var STATISTIC_BOX = null;
 var INIT_BALANCE = 0;
 var TAKE_PROFIT_AMOUNT = 100;
 var STOP_LOSS_AMOUNT = 1000;
@@ -9,22 +10,28 @@ var PROFIT_LOSS_AMOUNT = 0;
 
 var STRATEGY_CODE = 0; // Default strategy
 var INIT_BET_AMOUNT = 0.0001;
-//var INIT_PAYOUT = '2x';
+var INIT_PAYOUT = '2x';
 var FIRST_TIME_FLAG = true;
 var AUTO_FLAG = false;
 var MULTIPLYER = 2;
 var CUR_AMOUNT = 0;
+var LOSES_LIMIT_TO_MULTIPLY = 0;
+var MULTIPLYER_AFTER_LOSES_LIMIT = 2;
+
+// STEP (Strategy wins-switch)
+var WINS_SWITCH = false;
+var WINS_LIMIT_TO_SWITCH = 0;
+var WINS_SWITCH_COUNTER = 0;
 
 // Counter
-// var HIGHEST_WIN_COUNTER = 0;
-// var HIGHEST_LOSE_COUNTER = 0;
-// var NONSTOP_WIN_COUNTER = 0;
-// var NONSTOP_LOSE_COUNTER = 0;
-// var WIN_COUNTER = 0;
-// var LOOSE_COUNTER = 0;
+// var HIGHEST_NONSTOP_WIN_COUNTER = 0;
+// var HIGHEST_NONSTOP_LOSE_COUNTER = 0;
+var NONSTOP_WIN_COUNTER = 0;
+var NONSTOP_LOSE_COUNTER = 0;
+var WINS_COUNTER = 0;
+var LOSES_COUNTER = 0;
 
-// Stratey 1 when STRATEGY_CODE = 1 (424 Strategy)
-var s1LosesCounter = 0;
+// Stratey 1 when STRATEGY_CODE = 1 (Strategy 424)
 var s1LosesLimit = 2;
 var s1Multiplier = 2;
 var s1Payout = '4x';
@@ -32,11 +39,31 @@ var s1Payout = '4x';
 init();
 
 function init() {
+  // Statistics
+  STATISTIC_BOX = $('#input-group-addona').parent().parent().prev();
+  STATISTIC_BOX.addClass('text-center');
+  STATISTIC_BOX.html('Phiên bản: ' + VERSION_NAME);
+
   $("#input-group-addona").off();
   $("#input-group-addona").attr('title', 'Start ZBot');
-  $("#input-group-addona").text(VERSION_NAME + ' - START');
+  $("#input-group-addona").text('START Normal');
   $("#input-group-addona").click(function() {
     AUTO_FLAG = true;
+    STRATEGY_CODE = 0;
+    setStrategyName('Chiến thuật normal');
+    runAuto();
+  });
+
+  // Add Winswitch button
+  var winSwitchBtn = $("#input-group-addona").clone();
+  winSwitchBtn.prop('id', '#wins-switch');
+  $("#input-group-addona").after(winSwitchBtn);
+  winSwitchBtn.attr('title', 'Chien thuat Winswith: Chuyen doi chien thuat sau x lan thang');
+  winSwitchBtn.text('Winswitch');
+  winSwitchBtn.click(function() {
+    WINS_SWITCH = true;
+    AUTO_FLAG = true;
+    setStrategyName('Chiến thuật Winswitch');
     runAuto();
   });
 
@@ -49,24 +76,47 @@ function init() {
   });
 
   $("#input-group-addonb").off();
-  $("#input-group-addonb").attr('title', 'Chien thuat 424 - Strategy 424');
+  $("#input-group-addonb").attr('title', 'Chien thuat 424');
   $("#input-group-addonb").text('424');
   $("#input-group-addonb").click(function() {
-    STRATEGY_CODE = 1;
-    $('#mfpayoutmul').val(s1Payout);
-    $('#mfpayoutmul').keyup();
+    switchTo424Strategy();
     alert('Da chon chien thuat 424');
     AUTO_FLAG = true;
+    setStrategyName('Chiến thuật 424');
     runAuto();
   });
 
   setupAuto();
+
+  alert("Cập nhật V1.6:\n- Thêm chức năng đặt chuỗi thua gấp thếp\n- Thêm chiến thuật Winswitch\n- Thêm chức năng thống kê");
+}
+
+function setStrategyName(strategyName) {
+  STATISTIC_BOX.html('Phiên bản: ' + VERSION_NAME +
+    '</br><strong>' + strategyName + '</strong>'
+  );
+
+  // Create win-lose-box
+  $('<div/>', {
+    id: 'win-lose-box'
+  }).appendTo(STATISTIC_BOX);
+}
+
+function updateStatisticBox() {
+  var html = '</br>Tổng Wins: ' + WINS_COUNTER + '</br>' +
+    'Tổng Loses: ' + LOSES_COUNTER + '</br>' +
+    'Chuỗi Win: ' + NONSTOP_WIN_COUNTER + '</br>' +
+    'Chuỗi Lose: ' + NONSTOP_LOSE_COUNTER + '</br>' +
+    '</br>';
+
+  $('#win-lose-box').html(html);
 }
 
 function stopAuto() {
   // $('#mfInputAmount').val(0);
-  // AUTO_FLAG = false;
-  // FIRST_TIME_FLAG = true;
+  AUTO_FLAG = false;
+  FIRST_TIME_FLAG = true;
+  WINS_SWITCH = false;
   location.reload();
 }
 
@@ -75,20 +125,26 @@ function runAuto() {
   $("#input-group-addona").off(); // Disable "Start"
   $('#balancesx').attr('title', 'Tac gia: m.me/zickieloox');
 
-  if (+$('#mfInputAmount').val() > 0) {
-    INIT_BET_AMOUNT = +$('#mfInputAmount').val();
+  if (parseFloat($('#mfInputAmount').val()) > 0) {
+    INIT_BET_AMOUNT = parseFloat($('#mfInputAmount').val());
     CUR_AMOUNT = INIT_BET_AMOUNT;
   }
 
-  //INIT_PAYOUT = $('#mfpayoutmul').val();
-  INIT_BALANCE = +$('#balancesx').text();
+  INIT_PAYOUT = $('#mfpayoutmul').val();
+  INIT_BALANCE = parseFloat($('#balancesx').text());
 
-  if(STRATEGY_CODE == 0) {
-    MULTIPLYER = +prompt('Đặt multi = ', 2);
+  if (STRATEGY_CODE == 0) {
+    MULTIPLYER = parseFloat(prompt('Đặt hệ số gấp thếp - multi1: ', 2));
+    LOSES_LIMIT_TO_MULTIPLY = parseInt(prompt('Gấp thếp sau bao nhiêu chuỗi thua liên tiếp : (phải >= 2, mặc định = 1)', 1));
+    MULTIPLYER_AFTER_LOSES_LIMIT = parseFloat(prompt('Đặt hệ số gấp thếp sau khi vượt quá chuỗi thua liên tiếp - multi2: ', 2));
+
+    if (WINS_SWITCH) {
+      WINS_LIMIT_TO_SWITCH = parseInt(prompt('Chuyển đổi chiến thuật sau bao nhiêu lần thắng?', 1))
+    }
   }
 
-  TAKE_PROFIT_AMOUNT = +prompt('Đặt take-profit ($) = ', 10);
-  STOP_LOSS_AMOUNT = -prompt('Đặt stop-loss ($) = ', 100);
+  TAKE_PROFIT_AMOUNT = parseFloat(prompt('Đặt take-profit ($) : ', 10));
+  STOP_LOSS_AMOUNT = 0 - parseFloat(prompt('Đặt stop-loss ($) : ', 100));
 
   $("#input-group-addonb").off(); // Disable "424"
   $("#input-group-addonb").attr('title', 'Take-profit / Stop-loss');
@@ -96,6 +152,18 @@ function runAuto() {
     " / < " + STOP_LOSS_AMOUNT
   );
   $("#btnplaymb").click();
+}
+
+function resetToNormalStrategy() {
+  STRATEGY_CODE = 0;
+  $('#mfpayoutmul').val(INIT_PAYOUT);
+  $('#mfpayoutmul').keyup();
+}
+
+function switchTo424Strategy() {
+  STRATEGY_CODE = 1;
+  $('#mfpayoutmul').val(s1Payout);
+  $('#mfpayoutmul').keyup();
 }
 
 function resetBetAmount() {
@@ -214,6 +282,7 @@ function setupAuto() {
 
               })
               .fail(function(data) {
+                AUTO_FLAG = false;
                 //alert(data.responseJSON.message);
                 alert('Tài khoản của bạn chưa được active!');
                 location.reload();
@@ -222,39 +291,77 @@ function setupAuto() {
             FIRST_TIME_FLAG = false;
           }
 
+          // Calculate
+          if (data.isWin > 0) {
+            WINS_COUNTER++;
+            NONSTOP_WIN_COUNTER++;
+            // if(NONSTOP_WIN_COUNTER > HIGHEST_NONSTOP_WIN_COUNTER) {
+            //   HIGHEST_NONSTOP_WIN_COUNTER = NONSTOP_WIN_COUNTER;
+            //   NONSTOP_WIN_COUNTER =0;
+            // }
+
+            NONSTOP_LOSE_COUNTER = 0; // reset
+
+            WINS_SWITCH_COUNTER++;
+          } else {
+            LOSES_COUNTER++;
+            NONSTOP_LOSE_COUNTER++;
+            // if(NONSTOP_LOSE_COUNTER > HIGHEST_NONSTOP_LOSE_COUNTER) {
+            //   HIGHEST_NONSTOP_LOSE_COUNTER = NONSTOP_LOSE_COUNTER;
+            //   NONSTOP_LOSE_COUNTER =0;
+            // }
+
+            NONSTOP_WIN_COUNTER = 0; // reset
+          }
+
           // Bet based on custom strategy
           if (STRATEGY_CODE == 0) {
             if (data.isWin > 0) {
-              // WIN_COUNTER++;
-              // NONSTOP_WIN_COUNTER++;
-              // if(NONSTOP_WIN_COUNTER > HIGHEST_WIN_COUNTER) {
-              //   HIGHEST_WIN_COUNTER = NONSTOP_WIN_COUNTER;
-              //   NONSTOP_WIN_COUNTER =0;
-              // }
               resetBetAmount();
             } else {
-              // LOOSE_COUNTER++;
-              // NONSTOP_LOSE_COUNTER++;
-              // if(NONSTOP_LOSE_COUNTER > HIGHEST_LOSE_COUNTER) {
-              //   HIGHEST_LOSE_COUNTER = NONSTOP_LOSE_COUNTER;
-              //   NONSTOP_LOSE_COUNTER =0;
-              // }
-              multiplyBetAmount(MULTIPLYER);
+              // Default: LOSES_LIMIT_TO_MULTIPLY < 2
+              if (LOSES_LIMIT_TO_MULTIPLY >= 2) {
+                if (NONSTOP_LOSE_COUNTER == LOSES_LIMIT_TO_MULTIPLY) {
+                  multiplyBetAmount(MULTIPLYER);
+                } else if(NONSTOP_LOSE_COUNTER > LOSES_LIMIT_TO_MULTIPLY) {
+                  multiplyBetAmount(MULTIPLYER_AFTER_LOSES_LIMIT);
+                }
+              } else {
+                multiplyBetAmount(MULTIPLYER);
+              }
+
             }
+
           } else if (STRATEGY_CODE == 1) {
             if (data.isWin > 0) {
               resetBetAmount();
-              s1LosesCounter = 0; // reset
             } else {
-              s1LosesCounter++;
-              if (s1LosesCounter >= s1LosesLimit) {
+              if (NONSTOP_LOSE_COUNTER >= s1LosesLimit) {
                 multiplyBetAmount(s1Multiplier);
-                s1LosesCounter = 0; // reset
+                NONSTOP_LOSE_COUNTER = 0; // reset
               }
+
+            }
+
+          }
+
+          // Switch the strategy after x wins
+          if (WINS_SWITCH) {
+            if (WINS_SWITCH_COUNTER >= WINS_LIMIT_TO_SWITCH) {
+              if (STRATEGY_CODE == 0) {
+                switchTo424Strategy();
+              } else if (STRATEGY_CODE == 1) {
+                resetToNormalStrategy();
+              }
+
+              WINS_SWITCH_COUNTER = 0; // reset
             }
           }
 
+
+          // Update balance and profit-loss
           var curBalance = data.total_thisUser_profit;
+          //console.log(curBalance);
           if (curBalance > INIT_BALANCE) {
             PROFIT_LOSS_AMOUNT = curBalance - INIT_BALANCE;
             $('#balancesx').html(curBalance + ' / Profit: ' + PROFIT_LOSS_AMOUNT.toFixed(4));
@@ -276,20 +383,23 @@ function setupAuto() {
             }
           }
 
+          // Update statistics
+          updateStatisticBox();
 
-        }
-
-        // Author: m.me/zickieloox
-        if (AUTO_FLAG) {
-          setTimeout(function() {
-            $("#btnplaymb").click();
-          }, 2000);
         }
 
       },
       complete: function(jqXHR, textStatus) {
         $("#btnplaymb").removeAttr("disabled");
         $("#btnplaymb").html("ROLL DICE");
+
+        // Auto roll
+        if (AUTO_FLAG) {
+          setTimeout(function() {
+            $("#btnplaymb").click();
+          }, 2000);
+        }
+
         if (textStatus === "timeout") {
           $("#text_result_out").html('Request timed out!');
         }
